694030af18263dc7b5d1b0ed1fc9992b363a918a
Brandon Fulljames <bfullj@gmail.com>
2024-01-26 12:38:54 +0900

    Don't force add

--- .evertras/pre-commit.sh
+++ .evertras/pre-commit.sh
@@ -10,8 +10,8 @@ files=$(git diff --cached --name-only --diff-filter=ACMR | sed 's| |\\ |g')
 [ -z "$files" ] && exit 0
 
 echo "$files" | xargs npx prettier --ignore-unknown --write
-echo "$files" | xargs git add
-
 echo "$files" | xargs nixfmt
-
 go fmt ./...
+
+# Paranoid of this for now, just enforce it for the next pass
+#echo "$files" | xargs git add
