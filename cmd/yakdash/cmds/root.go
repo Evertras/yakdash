03679a9fb0a8bc4ef104ac8d36ffdfe3b4d436b6
Brandon Fulljames <bfullj@gmail.com>
2024-01-26 12:23:15 +0900

    Bubble tea boilerplate

--- cmd/yakdash/cmds/root.go
+++ cmd/yakdash/cmds/root.go
@@ -3,6 +3,8 @@ package cmds
 import (
 	"log"
 
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/evertras/yakdash/pkg/yakdash"
 	"github.com/spf13/cobra"
 )
 
@@ -13,6 +15,12 @@ func Execute() error {
 var rootCmd = &cobra.Command{
 	Use: "yakdash",
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Println("Hello from yakdash!")
+		model := yakdash.New()
+
+		p := tea.NewProgram(model)
+
+		if _, err := p.Run(); err != nil {
+			log.Fatal("Failed to run:", err)
+		}
 	},
 }
---
+++ pkg/yakdash/yakdash.go
@@ -0,0 +1,32 @@
+package yakdash
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+}
+
+func New() model {
+	return model{}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return "YAK (q to quit)"
+}
