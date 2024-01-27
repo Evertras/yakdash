7be3e5f8d8c69095b1da603ec12b4a706cf401bb
Brandon Fulljames <bfullj@gmail.com>
2024-01-27 10:19:49 +0900

    Use package explicitly

--- pkg/id/id_test.go
+++ pkg/id/id_test.go
@@ -1,12 +1,16 @@
-package id
+package id_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/evertras/yakdash/pkg/id"
+)
 
 func TestNew(t *testing.T) {
-	saw := map[ID]bool{}
+	saw := map[id.ID]bool{}
 
 	for i := 0; i < 1000; i++ {
-		id := New()
+		id := id.New()
 
 		if saw[id] {
 			t.Errorf("Saw duplicate ID %d", id)
